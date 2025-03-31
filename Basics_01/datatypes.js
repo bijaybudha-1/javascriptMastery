1bbf84ce37939c04d7bbffb1f9c8b62ef5b3a557
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2025-03-31 22:55:02 +0545

    Learn Data Types in Js

---
+++ Basics_01/datatypes.js
@@ -0,0 +1,47 @@
+// differnet data types in js
+/* 
+1. Number
+2. String
+3. Boolean
+4. Null
+5. undefined
+6. Symbol
+7. Object
+8. Bigint
+*/
+
+// 1. Number Datatypes:
+let number = 1;
+console.log(typeof(number));
+let age = 18;
+console.log(typeof age);
+
+
+// 2. String
+let username = "bijaybudha"
+console.log(typeof username);
+let months = "March"
+console.log(typeof(months));
+
+// 3. Boolean
+let isLogged = true;
+console.log(typeof(isLogged));
+let isAdmin = false;
+console.log(typeof isAdmin);
+
+// 4. Null
+let price = null
+console.log(typeof price);
+
+// 5. undefined
+
+let temAddress;
+console.log(typeof temAddress);
+
+// 6. Object
+
+let fruitsCategories = {
+    name: "Apple",
+    color: "Red"
+}
+console.log(typeof fruitsCategories);
---
+++ Basics_01/datatypes.md
@@ -0,0 +1,2 @@
+# Data Types:
+Data Types are made on the basis of how data are stored and how it is accessed.
