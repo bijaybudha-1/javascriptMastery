7ac41ffac2cc7506ccdd97a577d7f91aa67756f0
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2025-04-01 23:08:45 +0545

    Javascript Operators

---
+++ Basics_01/logical.js
@@ -0,0 +1,14 @@
+// && --> and
+// || --> or
+// ! --> reverse
+
+let isLoggedin = true;
+let isPaid = false;
+console.log(isLoggedin && isPaid); // false
+
+let isStudent = true;
+let isPresent = false;
+
+console.log(isStudent || isPresent); // true
+
+
---
+++ Basics_01/operation.js
@@ -0,0 +1,22 @@
+let sum = 5 + 4;
+
+let num1 = 100;
+let num2 = 25;
+let totalNumber = num1 + num2;
+
+// some opertion in js
+let addition = 10 + 15;
+let subtract = 15 - 7;
+let multiply = 3 * 4;
+let divide = 16 / 4;
+let reminder = 16 % 3;
+let expo = 2 ** 3;
+
+// Comparsion Operation
+let number1 = 10;
+let number2 = 10;
+let number3 = 5;
+console.log(number1 == number2); // true
+console.log(number1 != number3); // true
+console.log(number1 > number3); // true
+console.log(number1 < number3); // false
