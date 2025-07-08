74e9fcca7715b82f384b6da93e0f306d2038e5f5
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2025-07-08 18:53:34 +0545

    Adding DataTypes, Datatypes conversion and It's notes

---
+++ 01 Basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+let fullName = "Bijay budha"
+console.log(typeof fullName); // string
+
+let age = 18;
+console.log(typeof(age)); // number
+
+let isStudent = true;
+console.log(typeof isStudent); //boolean
+
+let balanced;
+console.log(typeof balanced); //undefined
+
+let payment = null;
+console.log(typeof payment); // object
+
+
---
+++ 01 Basics/03_typeConversion.js
@@ -0,0 +1,43 @@
+// string to Number
+let score = "23";
+console.log(score); // "23"
+let numberScore = Number(score);
+console.log(numberScore); // 23
+
+let username = "bijay123";
+console.log(username); // "bijay123"
+let typeUsername = Number(username);
+console.log(typeUsername); // NaN
+
+// Number to String
+let number = 34;
+let newNumber = String(number);
+console.log(newNumber); // String
+
+// boolean to string
+let boolenString = true;
+let newBoolenString = String(boolenString);
+console.log(newBoolenString); // true
+
+let perviousBoolen = false;
+let nextBooleanStrig = String(perviousBoolen)
+console.log(newBoolenString); // true
+
+
+// boolean to number
+let isStudent = true;
+let isStudentNumber = Number(isStudent);
+console.log(isStudentNumber); // 1
+
+
+let isLoggedIn = false
+let isLoggedInNumber = Number(isLoggedIn);
+console.log(isLoggedInNumber); // 0
+
+/*
+    short Note
+1 => true
+0 => false
+"" => false
+"bijay" => true
+*/ 
