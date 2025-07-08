767f74429c42bc87becf2293811d059000832dfd
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2025-07-08 17:28:06 +0545

    adding variable.js

---
+++ 01 Basics/varibale.js
@@ -0,0 +1,38 @@
+// Varibale declare
+var firstName = "Bijay";
+console.log(firstName);
+
+let lastName = "Budha";
+console.log(lastName);
+
+const username = 'bijaybudha';
+console.log(username);
+
+// variable reassign and redeclaration is allowed in var.
+console.log("var variable example");
+
+firstName = "Saiyish"; 
+console.log(firstName);
+var firstName = "Jennan";
+console.log(firstName);
+
+// Variable reassign is allowed in let
+console.log("let variable example");
+
+lastname = "Saud";
+console.log(lastName);
+
+
+// varibale redecalartion is not allowed in let
+// let lastname = "Rawal";
+console.log(lastName);
+
+
+// Variable reassignment and redecalartion is not allowed
+console.log("const variable example");
+
+username = "saiyishasaud";
+console.log(username);
+
+// const username = "saiyisha";
+// console.log(username);
